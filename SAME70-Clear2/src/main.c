eb20c868f6861f8f861b7e5e0b8e00d3158ca7e1
pedrooa <pedrooa@al.insper.edu.br>
2019-03-28 13:14:32 -0300

    Resolucao dos issues

--- SAME70-Clear2/src/main.c
+++ SAME70-Clear2/src/main.c
@@ -26,6 +26,7 @@
 /************************************************************************/
 
 #include "asf.h"
+#include "notes.h"
 
 /************************************************************************/
 /* defines                                                              */
@@ -174,98 +175,13 @@ void init(void)
 
 const int songspeed = 1.5; //Change to 2 for a slower version of the song, the bigger the number the slower the song
 //*****************************************
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
+
 
 //*****************************************
-int notes1[] = {       //Note of the song, 0 is a rest/pulse
+
+// "He's A Pirate" / "Pirates of the Caribbean" Theme Song Playing
+// Obtido em : https://github.com/xitangg/-Pirates-of-the-Caribbean-Theme-Song/blob/master/Pirates_of_the_Caribbean_-_Theme_Song.ino
+const int notes1[] = {       //Note of the song, 0 is a rest/pulse
    NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
    NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
    NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
@@ -318,7 +234,10 @@ int notes1[] = {       //Note of the song, 0 is a rest/pulse
    NOTE_D5, 0, 0, NOTE_C5, 0, 0,
    NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
 };
-int notes2[] = {       //Note of the song, 0 is a rest/pulse
+
+
+// Musica: Blue Bird / Notas obtidas de http://easymusic.altervista.org/blue-bird-guitar-sheet-music-naruto-theme-guitar-chords/
+const int notes2[] = {       //Note of the song, 0 is a rest/pulse
 	0,0,NOTE_E6,NOTE_A6,NOTE_B6,
 	NOTE_C7,NOTE_B6,NOTE_A6,
 	NOTE_A6,0,NOTE_E6,NOTE_A6,NOTE_B6,
@@ -359,7 +278,8 @@ int notes2[] = {       //Note of the song, 0 is a rest/pulse
 	NOTE_A6,NOTE_A6,0
 };
 
-int notes3[] = {       //Note of the song, 0 is a rest/pulse
+// Musica: Jingle Bells / Notas obtidas de https://www.hackster.io/joshi/piezo-christmas-songs-fd1ae9
+const int notes3[] = {       //Note of the song, 0 is a rest/pulse
   NOTE_E5, NOTE_E5, NOTE_E5,
   NOTE_E5, NOTE_E5, NOTE_E5,
   NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
@@ -374,7 +294,7 @@ int notes3[] = {       //Note of the song, 0 is a rest/pulse
 
 //int songs[][] ={notes1,notes2,notes3};
 //*****************************************
-int duration1[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
+const int duration1[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
   125, 125, 250, 125, 125, 
   125, 125, 250, 125, 125,
   125, 125, 250, 125, 125,
@@ -427,7 +347,8 @@ int duration1[] = {         //duration of each note (in ms) Quarter Note is set 
   250, 125, 375, 250, 125, 375,
   125, 125, 125, 125, 125, 500
 };
-int duration2[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
+
+const int duration2[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
 	500,125,125,125,125,
 	500,375,125,
 	500,125,125,125,125,
@@ -467,7 +388,7 @@ int duration2[] = {         //duration of each note (in ms) Quarter Note is set 
 	125,375,500
 };
 
-int duration3[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
+const int duration3[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
 125, 125, 250, 125, 125, 250, 125, 125, 125, 125, 500, 125, 125, 125, 125, 125, 125, 125, 62, 62, 125, 125, 125, 125, 250, 250
 };
 
@@ -483,9 +404,9 @@ void tone(long noteFreq,long duration){
 		}
 		
 		
-		pio_set(PIOA,BUZ_PIO_IDX_MASK);
+		pio_set(BUZ_PIO,BUZ_PIO_IDX_MASK);
 		delay_us(t);
-		pio_clear(PIOA, BUZ_PIO_IDX_MASK);
+		pio_clear(BUZ_PIO, BUZ_PIO_IDX_MASK);
 		delay_us(t);
 	}
 
